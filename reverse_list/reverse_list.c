c9ace1d6851987c6cb04792a2d2bab72ab8f39d6
bingtaoli <1464738780@qq.com>
2017-05-12 20:59:17 +0800

    Improve reverse list

--- reverse_list/reverse_list.c
+++ reverse_list/reverse_list.c
@@ -3,44 +3,29 @@
 
 /**
  * 简悦云风 微博http://weibo.com/2388714105/DjvJZC1Qc?from=page_1005052388714105_profile&wvr=6&mod=weibotime&type=comment
- * 最近为招应届生出了道笔试编程题,翻转一个单向链表.结果 7,8 个人里没有一个人可以正确的写出 O(1) 空间复杂度 O(n) 时间复杂度的程序. 搞得我都怀疑人生了. 刚才自己试了一下, 不就是十几行大白话的 C 程序吗? 真的有那么难?
+ * 最近为招应届生出了道笔试编程题,翻转一个单向链表.结果 7,8 个人里没有一个人可以正确的写出 O(1) 空间复杂度 O(n) 时间复杂度的程序. 搞得我都怀疑人生了.
+ * 刚才自己试了一下, 不就是十几行大白话的 C 程序吗? 真的有那么难?
  * */
 
-typedef struct list_tag {
-	struct list_tag *next;
+typedef struct node_t {
 	int value;
-} List;
+	struct node_t *next;
+} Node;
 
-List * reverse_list(List *list){
-	List *first = list;
-	if (first->next == NULL){
-		return first;
+Node * reverse_list(Node *head){
+	Node *slow = NULL;
+	Node *fast = head;
+	Node *faster = NULL;
+	while (fast){
+		faster = fast->next;
+		fast->next = slow;
+		slow = fast;
+		fast = faster;
 	}
-	List *second = first->next;
-	if (first->next->next == NULL){
-		second->next = NULL;
-		first->next = NULL;
-		return second;
-	}
-	first->next = NULL;
-	while (second->next != NULL){
-		List *prev = first;
-		first = second;
-		second = second->next;
-		first->next = prev;
-	}
-	second->next = first;
-	return second;
+	return slow;
 }
 
-List *init_list(int value){
-	List *p = (List *)malloc(sizeof(List));
-	p->next = NULL;
-	p->value = value;
-	return p;
-}
-
-void dump_list(List *p){
+void dump_list(Node *p){
 	while (p != NULL){
 		printf("%d ", p->value);
 		p = p->next;
@@ -48,27 +33,18 @@ void dump_list(List *p){
 	printf("\n");
 }
 
-void free_list(List *p){
-	while (p != NULL){
-		List *prev = p;
-		p = p->next;
-		free(prev);
-	}
-}
-
 int main(){
-	List *first = init_list(1);
-	List *second = init_list(2);
-	List *third = init_list(3);
-	List *fourth = init_list(4);
-	List *fifth = init_list(5);
-	first->next = second;
-	second->next = third;
-	third->next = fourth;
-	fourth->next = fifth;
-	dump_list(first);
-	List *result = reverse_list(first);
+	Node first = {1, NULL};
+	Node second = {2, NULL};
+	Node third = {3, NULL};
+	Node fourth = {4, NULL};
+	Node fifth = {5, NULL};
+	first.next = &second;
+	second.next = &third;
+	third.next = &fourth;
+	fourth.next = &fifth;
+	dump_list(&first);
+	Node *result = reverse_list(&first);
 	dump_list(result);
-	free_list(result);
 	return 0;
 }
